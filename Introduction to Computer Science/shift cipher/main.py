9bcddb7147127112a8638dfd2081dfffded4be6c
michaldudek15 <michalpaweldudek@gmail.com>
2022-11-05 22:29:11 +0100

    finished first version

--- Introduction to Computer Science/shift cipher/main.py
+++ Introduction to Computer Science/shift cipher/main.py
@@ -1,5 +1,11 @@
 import string
 
+# english message example with shift 3:
+# QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD
+
+# polish message example with shift -3:
+# OHBÓŻ DĆFĄ, EKTRP ŚZŃM YŹSŁ L UAGWĘ INCJ
+
 
 def shift(text, move, alphabet):
     move %= len(alphabet)
@@ -15,17 +21,17 @@ while True:
     if len(option) == 1 and (option == "1" or option == "2"):
         break
     else:
-        print('\033[93m' + 'please type either 1 or 2 on the keyboard to continue\n' + '\033[0m')
+        print('\033[93m' + 'please type either 1 or 2 to continue\n' + '\033[0m')
         continue
 
 print("now type the message you want to cipher:")
 message = input().lower()
 print("choose shift value")
-shift_value = input()
+shift_value = int(input())
 
 if option == "1":
-    shift(message, int(shift_value), string.ascii_lowercase)
+    shift(message, shift_value, string.ascii_lowercase)
 elif option == "2":
-    shift(message, int(shift_value), "aąbcćdeęfghijklłmnńoóprsśtuwyzźż")
+    shift(message, shift_value, "aąbcćdeęfghijklłmnńoóprsśtuwyzźż")
 else:
     print("something went wrong :((")
