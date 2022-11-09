b6ed029792d9d02a85edfe5c9795f387b23326ee
michaldudek15 <michalpaweldudek@gmail.com>
2022-11-09 16:50:27 +0100

    some progress

--- Introduction to Computer Science/number systems/main2.py
+++ Introduction to Computer Science/number systems/main2.py
@@ -1,23 +1,47 @@
-inputBase = int(input('choose the base of the system for your input number ( [2, 16] ): '))
+import string
+
+while True:
+    inputBase = int(input('choose the base of the system for your input number ( [2, 16] ): '))
+    if 1 < inputBase < 17:
+        break
+    if not str(inputBase).isdigit():
+        print("please use numbers from 2 to 16")
+        continue
+
 inputNumber = input('what number do you want to convert? ')
-outputBase = int(input('choose the base of the system you want to convert to: ( [2, 16] ) '))
+
+while True:
+    outputBase = int(input('choose the base of the system you want to convert to: ( [2, 16] ) '))
+    if 1 < outputBase < 17:
+        break
+    else:
+        print("please use numbers form 2 to 16")
+
 outputNumber = ''
 length = len(inputNumber)
+decimalNumber = 0
 
-if ord(inputNumber[length - 1]) > 64:
+
+if 64 < ord(inputNumber[length - 1]) < 71:
     currentDigit = ord(inputNumber[length - 1]) - 55
-else:
+    decimalNumber = currentDigit * pow(int(inputBase), 0)
+elif 47 < ord(inputNumber[length - 1]) < 58:
     currentDigit = int(inputNumber[length - 1])
-
-decimalNumber = currentDigit * pow(int(inputBase), 0)
+    decimalNumber = currentDigit * pow(int(inputBase), 0)
+else:
+    exit("please use only digits (0-9) or letters {A, B, C, D, E, F}")
 
 
 for i in range(1, length):
-    currentDigit = int(inputNumber[length - 1 - i])
+    if ord(inputNumber[length - 1 - i]) > 64:
+        currentDigit = ord(inputNumber[length - 1 - i]) - 55
+    else:
+        currentDigit = int(inputNumber[length - 1 - i])
     if chr(currentDigit) < '65':
         decimalNumber += currentDigit * pow(inputBase, i)
     else:
         currentDigit = int(chr(currentDigit - 55))
         decimalNumber += currentDigit * pow(inputBase, i)
+
 
 print("result: " + str(decimalNumber))
