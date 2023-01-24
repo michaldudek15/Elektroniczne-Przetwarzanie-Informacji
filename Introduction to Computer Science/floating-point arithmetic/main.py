6633c9916d2e88906d4fa057017825d6d1c43030
michaldudek15 <michalpaweldudek@gmail.com>
2023-01-24 15:18:24 +0100

    task agenda

---
+++ Introduction to Computer Science/floating-point arithmetic/main.py
@@ -0,0 +1,9 @@
+# 1. pobierz liczbę
+# 2. zamien liczbę na dwójkowy
+# 3. znormalizuj z cechą dziesiętną
+# 4. zamien cechę dziesiętną na binarną
+# 5. wypisz mantysę UZP
+# 6. wypisz cechę ODW
+# 7. wypisz cały rejestr
+
+
