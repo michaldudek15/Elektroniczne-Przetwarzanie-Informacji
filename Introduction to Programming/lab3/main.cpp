ebb492a40e9c590bbe6ccf7381dc0df0a55c729f
michaldudek <michalpaweldudek@gmail.com>
2023-03-13 14:28:38 +0100

    finished first 9 tasks

--- Introduction to Programming/lab3/main.cpp
+++ Introduction to Programming/lab3/main.cpp
@@ -13,6 +13,31 @@ public:
     void zadanie3();
     void zadanie4();
     void zadanie5();
+    void zadanie6();
+    void zadanie7();
+    void zadanie8();
+    void zadanie9();
+    //void zadanie10();
+    //void zadanie11();
+    //void zadanie12();
+    //void zadanie13();
+    //void zadanie14();
+    //void zadanie15();
+    //void zadanie16();
+    //void zadanie17();
+    //void zadanie18();
+    //void zadanie19();
+    //void zadanie20();
+    //void zadanie21();
+    //void zadanie22();
+    //void zadanie23();
+    //void zadanie24();
+    //void zadanie25();
+    //void zadanie26();
+    //void zadanie27();
+    //void zadanie28();
+    //void zadanie29();
+    //void zadanie30();
 };
 
 lab3::lab3() {
@@ -21,6 +46,31 @@ lab3::lab3() {
     zadania[2] = &lab3::zadanie3;
     zadania[3] = &lab3::zadanie4;
     zadania[4] = &lab3::zadanie5;
+    zadania[5] = &lab3::zadanie6;
+    zadania[6] = &lab3::zadanie7;
+    zadania[7] = &lab3::zadanie8;
+    zadania[8] = &lab3::zadanie9;
+    //zadania[9] = &lab3::zadanie10;
+    //zadania[10] = &lab3::zadanie11;
+    //zadania[11] = &lab3::zadanie12;
+    //zadania[12] = &lab3::zadanie13;
+    //zadania[13] = &lab3::zadanie14;
+    //zadania[14] = &lab3::zadanie15;
+    //zadania[15] = &lab3::zadanie16;
+    //zadania[16] = &lab3::zadanie17;
+    //zadania[17] = &lab3::zadanie18;
+    //zadania[18] = &lab3::zadanie19;
+    //zadania[19] = &lab3::zadanie20;
+    //zadania[20] = &lab3::zadanie21;
+    //zadania[21] = &lab3::zadanie22;
+    //zadania[22] = &lab3::zadanie23;
+    //zadania[23] = &lab3::zadanie24;
+    //zadania[24] = &lab3::zadanie25;
+    //zadania[25] = &lab3::zadanie26;
+    //zadania[26] = &lab3::zadanie27;
+    //zadania[27] = &lab3::zadanie28;
+    //zadania[28] = &lab3::zadanie29;
+    //zadania[29] = &lab3::zadanie30;
 }
 
 void lab3::call_by_index(int index) {
@@ -74,10 +124,56 @@ void lab3::zadanie4() {
 }
 
 void lab3::zadanie5() {
+    int i = 97;
+    while (i < 123) {
+        printf("%c: %d\n", i, i);
+        i++;
+    }
+}
+
+void lab3::zadanie6() {
     char litera;
-    cout << "kod ASCII jakiej litery chcesz sprawdzić? ";
+    cout << "kod ASCII jakiego znaku chcesz sprawdzić? ";
     cin >> litera;
     printf("%d", litera);
+}
+
+void lab3::zadanie7() {
+    int znak;
+    cout << "podaj wartość znaku ASCII: ";
+    cin >> znak;
+    printf("ten znak odpowiada: %c", znak);
+}
+
+void lab3::zadanie8() {
+    int rok;
+    cout << "podaj swój rok urodzenia: ";
+    cin >> rok;
+    if (rok > 2005) {
+        cout << "nie jesteś osobą pełnoletnią!" << endl;
+    }
+    else if (rok < 1913) {
+        cout << "wydaje mi się, że nie żyjesz" << endl;
+    }
+    else{
+        cout << "jesteś osobą pełnoletnią!" << endl;
+    }
+}
+
+void lab3::zadanie9() {
+    int liczba;
+    int ustalona = 36;
+
+    cout << "jaką liczbę wybierasz? ";
+    cin >> liczba;
+
+    if (liczba == ustalona) {
+        cout << "brawo, wygrywasz!" << endl;
+    }
+    else {
+        cout << "nie udało ci się trafić :((" << endl;
+    }
+
 }
 
 int main() {
