df483a410b287d2712dff1c5aa83d04a9b84a8b5
michaldudek <michalpaweldudek@gmail.com>
2023-05-12 09:19:19 +0200

    finished

--- Introduction to Programming/Dzien niepodleglosci/main.cpp
+++ Introduction to Programming/Dzien niepodleglosci/main.cpp
@@ -3,8 +3,61 @@
 
 using namespace std;
 
+bool hack (string &atak) {
+
+    for (int i = (atak.size() - 2); i >= 0; i--) {
+        if (atak[i] == 'L' && atak[i+1] == 'S') {
+            atak[i] = 'S';
+            atak[i+1] = 'L';
+            return true; // jesli sie uda zhakować zwraca true
+        }
+    }
+    return false; // jesli sie nie uda zwraca false
+}
+
+int test (string atak) {
+    int power = 1;
+    int damage = 0;
+
+    for (int i = 0; i < atak.size(); i++) {
+        if (atak[i] == 'L') power *= 2;
+        if (atak[i] == 'S') damage += power;
+    }
+    return damage;
+}
+
 int main() {
 
+   int sesje;
+   cin >> sesje;
+
+   while (sesje) {
+
+       int HP;
+       cin >> HP;
+
+       string atak;
+       cin >> atak;
+
+       int licznik = 0;
+       bool mozliwe = true;
+
+
+       while (test(atak) > HP) {
+           if(!hack(atak)) {
+           mozliwe = false;
+           break;
+           }
+           else {
+               licznik++;
+           }
+       }
+
+       if (mozliwe) cout << licznik << endl;
+       else cout << "NIEMOZLIWE" << endl;
+
+       sesje--;
+   }
 
     return 0;
 }
