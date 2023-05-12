e742d65d487fc586a11a99d16f8a570de3f3ae13
michaldudek <michalpaweldudek@gmail.com>
2023-05-12 13:23:10 +0200

    finished

--- Introduction to Programming/Samochody/main.cpp
+++ Introduction to Programming/Samochody/main.cpp
@@ -3,14 +3,40 @@
 
 using namespace std;
 
+void wlamanie(bool opened[], int &cena, int index) {
+    cena += 500;
+    opened[index] = true;
+}
+
 int main() {
 
+    // wczytanie danych
+    int liczbaSamochodow;
+    cin >> liczbaSamochodow;
+    int samochody[liczbaSamochodow];
+    for (int i = 0; i < liczbaSamochodow; i++ ) {
+        cin >> samochody[i];
+    }
 
+    // pomocnicza tablica zapisujaca czy samochod jest zamkniety
+    bool opened[liczbaSamochodow];
+    for (int i = 0; i < liczbaSamochodow; i++ ) {
+        opened[i] = false;
+    }
 
+    int cena = 0;
+    for (int i = 0; i < liczbaSamochodow; i++) {
+        if (!opened[i]) {
+            wlamanie (opened, cena, i); // wlamanie do pierwszego samochodu (poczatek cyklu)
+            int klucz = samochody[i] - 1;
+            while(!opened[klucz]) { // cykl konczy sie przy probie otwarcia otwartego samochodu
+                opened[klucz] = true;
+                klucz = samochody[klucz] - 1;
+            }
+        }
+    }
 
-
-
-
+    cout << cena;
 
     return 0;
 }
