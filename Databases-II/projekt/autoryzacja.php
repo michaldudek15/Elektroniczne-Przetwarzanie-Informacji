61b29e36e0b5d369237955f9f9e49f522da54605
Michał Dudek <michalpaweldudek@gmail.com>
2024-01-25 21:07:28 +0100

    moving files

---
+++ Databases-II/projekt/autoryzacja.php
@@ -0,0 +1,13 @@
+<?php
+	// dane do wierzbie
+	// $dbhost='localhost';
+	// $dbuser='22_dudek';
+	// $dbpass='J0z5f4t5';
+	// $dbname='22_dudek';
+
+	// dane lokalne
+	$dbhost='localhost';
+	$dbuser='root';
+	$dbpass='zxcvbnm123';
+	$dbname='projekt_bazy_danych_2';
+?>
