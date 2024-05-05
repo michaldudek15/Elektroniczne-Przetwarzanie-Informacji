5af169b8a2012127c437594e843925ad86b1977c
Michał Dudek <michalpaweldudek@gmail.com>
2024-05-05 14:47:31 +0200

    Hello World! - spersonalizowane powitanie z templatką index.html.twig

--- Interactive-System/app/src/Controller/HelloController.php
+++ Interactive-System/app/src/Controller/HelloController.php
@@ -5,25 +5,35 @@
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 /**
- * HelloController class.
+ * Class HelloController.
  */
-class HelloController
+#[Route('/hello')]
+class HelloController extends AbstractController
 {
     /**
      * Index action.
      *
+     * @param string $name User input
+     *
      * @return Response HTTP response
      */
-    #[Route('/hello', name: 'hello_index', methods: 'GET')]
-    public function index(Request $request): Response
+    #[Route(
+        '/{name}',
+        name: 'hello_index',
+        requirements: ['name' => '[a-zA-Z]+'],
+        defaults: ['name' => 'World'],
+        methods: 'GET'
+    )]
+    public function index(string $name): Response
     {
-        $name = $request->query->getAlnum('name', 'World');
-
-        return new Response('Hello '.$name.'!');
+        return $this->render(
+            'hello/index.html.twig',
+            ['name' => $name]
+        );
     }
 }
