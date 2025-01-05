f03b62b730454642879c961d657af47594b661ff
Michał Dudek <michalpaweldudek@gmail.com>
2025-01-05 20:03:31 +0100

    finished

---
+++ graphical-interface/02.01-liczby/liczby.py
@@ -0,0 +1,13 @@
+# zastąpić odpowiednią funkcją wczytującą dane od użytkownika
+l = input ("podaj liczbę całkowitą: ")
+
+# Wypisze wprowadzoną liczbę
+print ("twoja liczba to: ", l)
+# Wypisze podwojoną
+print ("twoja liczba po podwojeniu to: ", int(l)*2)
+# Wypisze wiele razy
+print("powtarzając twoją liczbę 5 razy otrzymamy: ", 5*l)
+# Wypisze podzieloną przez 3
+print ("podzielona przez 3: ", int(l)/3)
+# Wypisze resztę z dzielenia
+print ("reszta z dzielenia przez 7 to: ", int(l)%7)
