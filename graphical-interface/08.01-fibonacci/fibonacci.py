e3b8d616043be13887a7809167ea6492f87a49e4
Michał Dudek <michalpaweldudek@gmail.com>
2025-01-08 20:50:28 +0100

    finished

---
+++ graphical-interface/08.01-fibonacci/fibonacci.py
@@ -0,0 +1,14 @@
+def fib_gen():
+    """
+    generuje kolejne liczby fibonacciego
+    """
+    a, b = 0, 1
+    while True:
+        yield a
+        a, b = b, a + b
+
+
+fib = fib_gen()
+
+for i in range(15):
+    print(next(fib))
