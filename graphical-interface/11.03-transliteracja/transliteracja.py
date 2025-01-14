cf33797270c2705704aa666fe1c6c002daf48aab
Michał Dudek <michalpaweldudek@gmail.com>
2025-01-14 14:26:31 +0100

    finished (need to add comments)

--- graphical-interface/11.03-transliteracja/transliteracja.py
+++ graphical-interface/11.03-transliteracja/transliteracja.py
@@ -1,40 +1,54 @@
 # Mapa transliteracji z rosyjskiego na łaciński
 translit_map = {
-    'А': 'A', 'а': 'a', 'Б': 'B', 'б': 'b', 'В': 'V', 'в': 'v',
-    'Г': 'G', 'г': 'g', 'Д': 'D', 'д': 'd', 'Е': 'E', 'е': 'e',
-    'Ё': 'Yo', 'ё': 'yo', 'Ж': 'Zh', 'ж': 'zh', 'З': 'Z', 'з': 'z',
-    'И': 'I', 'и': 'i', 'Й': 'Y', 'й': 'y', 'К': 'K', 'к': 'k',
-    'Л': 'L', 'л': 'l', 'М': 'M', 'м': 'm', 'Н': 'N', 'н': 'n',
-    'О': 'O', 'о': 'o', 'П': 'P', 'п': 'p', 'Р': 'R', 'р': 'r',
-    'С': 'S', 'с': 's', 'Т': 'T', 'т': 't', 'У': 'U', 'у': 'u',
-    'Ф': 'F', 'ф': 'f', 'Х': 'Kh', 'х': 'kh', 'Ц': 'Ts', 'ц': 'ts',
-    'Ч': 'Ch', 'ч': 'ch', 'Ш': 'Sh', 'ш': 'sh', 'Щ': 'Shch', 'щ': 'shch',
-    'Ы': 'Y', 'ы': 'y', 'Э': 'E', 'э': 'e', 'Ю': 'Yu', 'ю': 'yu',
-    'Я': 'Ya', 'я': 'ya', 'Ь': '', 'ь': '', 'Ъ': '', 'ъ': ''
+    'А': 'A', 'а': 'a', 
+    'Б': 'B', 'б': 'b', 
+    'В': 'V', 'в': 'v',
+    'Г': 'G', 'г': 'g', 
+    'Д': 'D', 'д': 'd', 
+    'Е': 'E', 'е': 'e',
+    'Ё': 'Ё', 'ё': 'ё', 
+    'Ж': 'Ž', 'ж': 'ž', 
+    'З': 'Z', 'з': 'z',
+    'И': 'I', 'и': 'i', 
+    'Й': 'J', 'й': 'j', 
+    'К': 'K', 'к': 'k',
+    'Л': 'L', 'л': 'l', 
+    'М': 'M', 'м': 'm', 
+    'Н': 'N', 'н': 'n',
+    'О': 'O', 'о': 'o', 
+    'П': 'P', 'п': 'p', 
+    'Р': 'R', 'р': 'r',
+    'С': 'S', 'с': 's', 
+    'Т': 'T', 'т': 't', 
+    'У': 'U', 'у': 'u',
+    'Ф': 'F', 'ф': 'f', 
+    'Х': 'H', 'х': 'h', 
+    'Ц': 'C', 'ц': 'c',
+    'Ч': 'Č', 'ч': 'č', 
+    'Ш': 'Š', 'ш': 'š', 
+    'Щ': 'Ŝ', 'щ': 'ŝ',
+    'Ь': '″', 'ь': '″', 
+    'Ы': 'Y', 'ы': 'y', 
+    'Э': 'È', 'э': 'è', 
+    'Ю': 'Û', 'ю': 'û',
+    'Я': 'Â', 'я': 'â', 
+    'Ъ': '’', 'ъ': '’'
 }
 
 def transliterate_ru_to_lat(text):
     """
-    Transliteruje rosyjski tekst na łaciński używając mapy translit_map.
+    transliteruje tekst zapiasny alfabetem rosyjskim na alfabet łaciński
     """
     result = []
     for char in text:
-        result.append(translit_map.get(char, char))  # Użyj mapy lub zostaw znak bez zmian
+        result.append(translit_map.get(char, char))
     return ''.join(result)
 
-def main():
-    # Odczytanie tekstu z pliku "ru.txt"
-    with open("ru.txt", "r", encoding="utf-8") as file:
-        input_text = file.read()
 
-    # Transliteracja tekstu
-    transliterated_text = transliterate_ru_to_lat(input_text)
+with open("ru.txt", "r", encoding="utf-8") as file:
+    input_text = file.read()
 
-    # Zapisanie transliterowanego tekstu do pliku "transliteracja.txt"
-    with open("transliteracja.txt", "w", encoding="utf-8") as file:
-        file.write(transliterated_text)
+transliterated_text = transliterate_ru_to_lat(input_text)
 
-    print("Transliteracja zakończona. Wynik zapisano w 'transliteracja.txt'.")
-
-if __name__ == "__main__":
-    main()
+with open("transliteracja.txt", "w", encoding="utf-8") as file:
+    file.write(transliterated_text)
