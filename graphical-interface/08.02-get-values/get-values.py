7ff8c4c5d2f4bc263676f897d64fcf299ea8f822
Michał Dudek <michalpaweldudek@gmail.com>
2025-01-15 17:21:38 +0100

    finished

--- graphical-interface/08.02-get-values/get-values.py
+++ graphical-interface/08.02-get-values/get-values.py
@@ -1,23 +1,7 @@
-"""
-Proszę napisać funkcję **get_values(iterator, n)**, zwracającą listę _n_ kolejnych wartości pobranych z iteratora. 
-Gdy wartości się skończą, należy odpowiednio skrócić zwracaną listę. 
-
-Funkcja powinna działać, jak poniżej:
-
-```
->>> i = iter(range(10))
->>> get_values(i, 3)
-[0, 1, 2]
->>> get_values(i, 5)
-[3, 4, 5, 6, 7]
->>> get_values(i, 4)
-[8, 9]
->>> get_values(i, 4)
-[]
-```
-"""
-
 def get_values(iterator, n):
+    """
+    funkcja zwraca listę kolejnych wartości pobranych z iteratora
+    """
     lista = []
     for x in range(n):
         value = next(iterator, None)
